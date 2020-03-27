7632188a7623b814bb5368754f5ae447a11e7c36
sowrd299 <ryan.ward46@gmail.com>
2020-03-26 18:41:35 -0700

    Adds help menu

--- gui.py
+++ gui.py
@@ -19,13 +19,17 @@ class GUI(tkinter.Frame):
 
     def create_widgets(self):
 
-        self.top_frame = tkinter.Frame(self)
+        self.top_frame = tkinter.Frame(self, bg=self.bg2)
         self.top_frame.pack(fill = tkinter.X)
+
+        # OPEN HELP MENU
+        self.help_button = tkinter.Button(self.top_frame, text="Help", command = self.open_help, bg=self.bg2, fg=self.fg, font=(self.font, 8))
+        self.help_button.pack(side = tkinter.LEFT)
 
         # DISPLAY CURRENT SUBTOTALS
         self.subtotals_v = tkinter.StringVar()
         self.subtotals = tkinter.Label(self.top_frame, textvariable=self.subtotals_v, bg=self.bg2, fg=self.fg, font=(self.font, 12), anchor=tkinter.W)
-        self.subtotals.pack(fill = tkinter.X)
+        self.subtotals.pack(side = tkinter.LEFT)#fill = tkinter.X)
 
         # THE MAIN NUMERIC DISPLAY
         self.number_v = tkinter.StringVar()
@@ -36,6 +40,32 @@ class GUI(tkinter.Frame):
         return str(am.get_number())
 
     def update(self, am):
-        self.subtotals_v.set("Subtotals: " + ", ".join(map(lambda x : str(x) , am.get_subtotals())))
+        self.subtotals_v.set(" Subtotals: " + ", ".join(map(lambda x : str(x) , am.get_subtotals())))
         self.number_v.set(self.get_number_disp(am))
 
+    def open_help(self):
+        window = tkinter.Toplevel(self)
+        help_gui = HelpGUI(window)
+
+
+class HelpGUI(GUI):
+
+    help_text = "\n".join([
+        'COMMANDS:',
+        '"x"/"c": Copy number to clipboard',
+        '"v": Paste number from clipboard',
+        '"C": Clear All/Complete reset',
+        'Backspace/Right Arrow: Delete least significant digit',
+        '"+", "-", "*", "/", "=", and number keys:',
+        '\tWork like an adding machine',
+        'Enter: Same as "="; numpad enter may not work'
+    ])
+    
+
+    def create_widgets(self):
+
+        self.help_label = tkinter.Label(self, text=self.help_text, bg=self.bg, fg=self.fg, font=(self.font, 14), justify=tkinter.LEFT)
+        self.help_label.pack()
+
+    def update(self):
+        pass
